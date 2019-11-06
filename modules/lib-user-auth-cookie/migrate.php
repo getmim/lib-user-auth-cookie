2f015efa9621a9cf2991122e50a1d82ac1193e77
Iqbal Fauzi <iqbalfawz@gmail.com>
2019-11-06 20:26:23 +0700

    Add table index

--- modules/lib-user-auth-cookie/migrate.php
+++ modules/lib-user-auth-cookie/migrate.php
@@ -9,14 +9,16 @@ return [
                     'unsigned' => true,
                     'primary_key' => true,
                     'auto_increment' => true
-                ]
+                ],
+                'index' => 1000
             ],
             'user' => [
                 'type' => 'INT',
                 'attrs' => [
                     'null' => false,
                     'unsigned' => true 
-                ]
+                ],
+                'index' => 2000
             ],
             'hash' => [
                 'type' => 'VARCHAR',
@@ -24,26 +26,30 @@ return [
                 'attrs' => [
                     'unique' => true,
                     'null' => false 
-                ]
+                ],
+                'index' => 3000
             ],
             'expires' => [
                 'type' => 'DATETIME',
                 'attrs' => [
                     'null' => false 
-                ]
+                ],
+                'index' => 4000
             ],
             'updated' => [
                 'type' => 'TIMESTAMP',
                 'attrs' => [
                     'default' => 'CURRENT_TIMESTAMP',
                     'update' => 'CURRENT_TIMESTAMP'
-                ]
+                ],
+                'index' => 5000
             ],
             'created' => [
                 'type' => 'TIMESTAMP',
                 'attrs' => [
                     'default' => 'CURRENT_TIMESTAMP'
-                ]
+                ],
+                'index' => 6000
             ]
         ]
     ]
